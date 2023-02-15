7a28ae0c7278ae59f13058a39a8019eafcff4adb
篠原智哉 <tomoya6032@yahoo.co.jp>
2023-02-16 06:40:20 +0900

    view、scss,モデル、コントローラーを整備した アプリケーションの大枠は出来上がったので、次回から表示される部分をコーディングしていく

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_14_213439) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_15_212420) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_14_213439) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_reactions_on_post_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "comments", "posts"
