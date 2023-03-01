25040d04ae3347e8e58ed1c6fac75ab7282cc8a0
篠原智哉 <tomoya6032@yahoo.co.jp>
2023-03-01 17:22:50 +0900

    一覧表示画面のデザインを整えた 画像が表示されないのはimageテーブルを作らないといけないようだ

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,15 @@
 class PostsController < ApplicationController
+ 
+  before_action :set_post, only: [:show]
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-    def index
+ 
+
+   def index
         @posts = Post.all
     end
     
     def show
-        @post = Post.find(params[:id])
+        # @post = Post.find(params[:id])
     end
     
     def new
@@ -13,8 +17,9 @@ class PostsController < ApplicationController
     end
     
     def create
-      @post = Post.new(post_params)
+      # @post = Post.new(post_params)
       @post = current_user.posts.build(post_params)
+        # @post = current_user.posts.new(post_params)
         if @post.save
           redirect_to @post, notice: "Post was successfully created."
         else
@@ -22,10 +27,17 @@ class PostsController < ApplicationController
        end
     end
     
+
+
     private
 
     def post_params
-      params.require(:post).permit(:image, :content).merge(user_id: current_user.id)
+      # params.require(:post).permit(:content, images: [])
+      params.require(:post).permit(:content, images_attributes: [:image]).merge(user_id: current_user.id)
+    end
+
+    def set_post
+      @post = Post.find(params[:id])
     end
     
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,23 @@
 class Post < ApplicationRecord
     belongs_to :user
 
-    has_many_attached :image
-    validates :image, presence: true
+    has_many_attached :images
+    validates :images, presence: true
     validates :content, presence: true
     mount_uploader :image, ImageUploader
     has_many :comments
     has_many :reactions
+
+    # def display_created_at
+    #   I18n.l(self.created_at, format: :default)
+    # end
+
+    # def author_name
+    #   user.display_name
+    # end
+
+    # def like_count
+    #   likes.count
+    # end
+
 end
